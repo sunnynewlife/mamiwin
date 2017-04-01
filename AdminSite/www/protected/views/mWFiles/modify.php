a99c5af9363d84aa9c120920a1cdd68b003d6e76
sunnynewlife <sunnynewlife@qq.com>
2017-04-01 11:01:40 +0800

    素材管理 ，编辑页面，上传本地图片

--- AdminSite/www/protected/views/mWFiles/modify.php
+++ AdminSite/www/protected/views/mWFiles/modify.php
@@ -7,6 +7,7 @@
 <script src="/static/summernote/summernote.js"></script>
 <script src="/static/summernote/lang/summernote-zh-CN.js"></script>
 
+
 <div class="well">
 	<ul class="nav nav-tabs">
 		<li class="active"><a href="#home" data-toggle="tab">请填写文件信息</a></li>
@@ -121,7 +122,29 @@
 	      	  height: 300,                 // set editor height
 	    	  minHeight: null,             // set minimum height of editor
 	    	  maxHeight: null,
-	    	  lang: 'zh-CN'
+	    	  lang: 'zh-CN',
+	    	  callbacks:{
+		    	  onImageUpload: function(files) {
+		    		  console.log("image upload...");
+		    		  var formData = new FormData();
+		              formData.append('file',files[0]);
+		              $.ajax({
+		                  url : '/mWFiles/uploadImage',//后台文件上传接口
+		                  type : 'POST',
+		                  data : formData,
+		                  processData : false,
+		                  contentType : false,
+		                  dataType:"json",
+		                  success : function(data) {
+			                  if(data.return_code==0){
+		                      	$('#summernote').summernote('insertImage',data.url,'img');
+			                  }else{
+				                  alert(data.message);
+			                  }
+		                  }
+		              });		    		  
+		    	  }
+				},
 	    	});	
 	}
   
@@ -132,4 +155,4 @@
 		}
 ?>           	
     });
-</script>    
+</script>
