b3462331b6b66bdf8263821c53f1277d43807f1a
yangenrui <yangenrui@ymq.me>
2017-04-24 22:51:51 +0800

    配置管理

--- AdminSite/www/protected/views/taskConfig/modify.php
+++ AdminSite/www/protected/views/taskConfig/modify.php
@@ -12,13 +12,13 @@
 			<form id="tab" method="post" action="" enctype="multipart/form-data">
 				<br/>
 				<label>配置项Key</label> 
-				<input type="text" maxlength="50" name="Confg_Key" value="<?php echo $TaskConfig["Confg_Key"];?>" class="input-xlarge" required="true" autofocus="true" />
+				<input type="text" maxlength="50" name="Confg_Key" value="<?php echo $TaskConfig["Confg_Key"];?>" class="input-xlarge" required="true" readonly="true" />
 				<br/>
 				<label>配置项Value</label> 
-				<input type="text" maxlength="50" name="Config_Value" value="<?php echo $TaskConfig["Config_Value"];?>" class="input-large" required="true" autofocus="true" />
+				<input type="text" maxlength="50" name="Config_Value" value="<?php echo $TaskConfig["Config_Value"];?>" class="input-xlarge" required="true" autofocus="true" />
 				<br/>
 				<label>配置项说明</label> 
-				<input type="text" maxlength="200" name="Config_Remark" value="<?php echo $TaskConfig["Config_Remark"];?>" class="input-large" required="true" autofocus="true" />
+				<input type="text" maxlength="200" name="Config_Remark" value="<?php echo $TaskConfig["Config_Remark"];?>" class="input-xlarge" required="true" autofocus="true" />
 				<br/>
 								
 				<input type="hidden" name="submit" value="1" />
