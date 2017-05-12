625d77fcc34ade8bd5e240fbb4f9db1e2d95935c
sunnynewlife <sunnynewlife@qq.com>
2017-05-12 22:17:36 +0800

    素材 外链

--- AdminSite/www/protected/models/UserInfoData.php
+++ AdminSite/www/protected/models/UserInfoData.php
@@ -29,25 +29,5 @@ class 	UserInfoData
 	}
 	
 	
-	// /**
-	//  * 获取所有的评测试题集
-	//  * @return [type] [description]
-	//  */
-	// public function getEvaluationQuestionsSetList()
-	// {
-	// 	$sql="select * from  Evaluation_Questions_Set ";
-	// 	$params=array();
-	// 	$EvQuestion=LunaPdo::GetInstance($this->_PDO_NODE_NAME)->query_with_prepare($sql,$params,PDO::FETCH_ASSOC);
-	// 	return (isset($EvQuestion) && is_array($EvQuestion) && count($EvQuestion)>0)?$EvQuestion:array();		
-	// }
-	
-	// public function updateEvQuestionSet($Set_Name,$Set_Qty,$Set_Type,$Remark,$Conditon_Child_Gender,$Condition_Parent_Gender,$Condition_Parent_Marriage,$Condition_Min_Age,$Condition_Max_age,$Condition_Only_Children,$IDX)
-	// {
-	// 	$sql="update Evaluation_Questions_Set set Set_Name=?,Set_Qty=?,Remark=?,Update_Time=now() where IDX=?";//Set_Type=?,Conditon_Child_Gender=?,Condition_Parent_Gender=?,Condition_Parent_Marriage=?,Condition_Min_Age=?,Condition_Max_age=?,Condition_Only_Children=?,
-	// 	$params=array($Set_Name,$Set_Qty,$Remark,$IDX);//$Set_Type,$Conditon_Child_Gender,$Condition_Parent_Gender,$Condition_Parent_Marriage,$Condition_Min_Age,$Condition_Max_age,$Condition_Only_Children,
-	// 	return LunaPdo::GetInstance($this->_PDO_NODE_NAME)->exec_with_prepare($sql,$params);
-	// }
 
-
-	
 }
